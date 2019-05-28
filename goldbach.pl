f3466c2d17e7aefd4b2ceead4fca6a4e8b30976e
Chaoyuuu <a0935292660@gmail.com>
2019-05-28 20:55:13 +0800

    fixed bugs

--- goldbach.pl
+++ goldbach.pl
@@ -1,7 +1,6 @@
 has_factor(N,L) :- N mod L =:= 0.
 has_factor(N,L) :- L * L < N, L2 is L + 2, has_factor(N,L2).
 
-
 is_prime(2).
 is_prime(3).
 is_prime(P) :- integer(P), P > 3, P mod 2 =\= 0, \+ has_factor(P,3).  
@@ -10,29 +9,29 @@ next_prime(P,P1) :- P1 is P + 2, is_prime(P1), !.
 next_prime(P,P1) :- P2 is P + 2, next_prime(P2,P1).
 
 goldbach(4,[2,2]).
-goldbach(6, [3, 3]).
 
-goldbach(N,L) :-
+search_goldbach(N,L) :-
+  N == 4 -> write("2 2"), nl; 
+  N == 6 -> write("3 3"), nl;
   N mod 2 =:= 0,
   N > 4,
-  goldbach(N,L,3).
+  if_goldbach(N,L,3).
 
-goldbach(N,[P,Q],P) :-
+if_goldbach(N,[P,Q],P) :-
   Q is N - P,
   is_prime(Q), P < Q ,
   write(P), write(" "), write(Q), nl,
   next_prime(P,P1),
-  goldbach(N,L,P1).
+  if_goldbach(N,L,P1).
+  
+if_goldbach(N,L,P) :- P > N/2, nl, !.
 
-
-goldbash(N,L,P) :- P > N/2, nl, !.
-goldbach(N,L,P) :- 
+if_goldbach(N,L,P) :- 
   P < N,
   next_prime(P,P1),
-  goldbach(N,L,P1).
+  if_goldbach(N,L,P1).
 
-q1 :- write("Input:"),
-            read(X), write("Output "), nl, 
-            goldbach(X, [A, B]).
-            % -> write("Output:"), write(A), write(" "), write(B); 
-            %nl.
+main :- 
+  read(X), write("Output "), nl, 
+  search_goldbach(X, [A, B]).
+
